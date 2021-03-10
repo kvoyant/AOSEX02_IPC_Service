397941382fdf018615562da01f604ab6b052ce62
김영호 <kvoyant@gmail.com>
2021-03-10 10:34:44 +0900

    init

---
+++ app/src/main/java/com/yhcomp/aosex02_ipc_service/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yhcomp.aosex02_ipc_service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
