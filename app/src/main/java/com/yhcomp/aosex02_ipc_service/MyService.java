1951137bd4aa7648e5223a3d03fd63b8f9d3cd32
김영호 <kvoyant@gmail.com>
2021-03-10 10:52:32 +0900

    원격서비스 생성 - project > rebuild 해야 Remote.Stub 정상 빌드

---
+++ app/src/main/java/com/yhcomp/aosex02_ipc_service/MyService.java
@@ -0,0 +1,48 @@
+package com.yhcomp.aosex02_ipc_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        Toast.makeText(this, "[RemoteServie] onBind 함수 호출", Toast.LENGTH_LONG).show();
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Toast.makeText(this, "[RemoteServie] onCrate 함수 호출", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "[RemoteServie] onDestroy 함수 호출", Toast.LENGTH_LONG).show();
+    }
+
+    RemoteStub.Stub mBinder = new RemoteStub.Stub() {
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+
+        }
+
+        @Override
+        public String getServerPackageName() throws RemoteException {
+            return null;
+        }
+    };
+
+}
