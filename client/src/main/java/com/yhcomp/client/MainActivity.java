f970a9afe57fdd1ae9b49e33e676436c78e76447
김영호 <kvoyant@gmail.com>
2021-03-10 14:59:43 +0900

    client 모듈 - bindService를 통한 원격 서비스 연결 시도 성공!

--- client/src/main/java/com/yhcomp/client/MainActivity.java
+++ client/src/main/java/com/yhcomp/client/MainActivity.java
@@ -2,10 +2,17 @@ package com.yhcomp.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yhcomp.aosex02_ipc_service.RemoteStub;
 
@@ -13,9 +20,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private RemoteStub mBinder = null;
     TextView text;
+    private String Tag = "MainActivity(client) ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.e("asdd", Tag + "onCreate() Bundle :" + savedInstanceState);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,6 +43,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        Log.e("asdd", Tag + "onClick() view.getId() :" + view.getId());
+
+        switch(view.getId()) {
+            case R.id.startbutton: {
+                Toast.makeText(this, "원격 서비스 실행", Toast.LENGTH_LONG).show();
+
+                Log.e("asdd", Tag + "   - bindService를 통한 원격 서비스 연결 시도");
+
+                Intent intent = new Intent();
+                ComponentName name = new ComponentName(
+                        "com.yhcomp.aosex02_ipc_service",//패키지명
+                        "com.yhcomp.aosex02_ipc_service.MyService" //원격 서비스 클래스명
+                );
+
+                intent.setComponent(name);
+
+                bindService(intent, srvConn, Context.BIND_AUTO_CREATE);
+                break;
+            }
+        }
 
     }
+
+    ServiceConnection srvConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.e("asdd", Tag + "onServiceConnected() ~ 연결");
+            Log.e("asdd", Tag + "   - componentName :" + componentName);
+            Log.e("asdd", Tag + "   - iBinder() :" + iBinder);
+
+            mBinder = RemoteStub.Stub.asInterface(iBinder);
+
+            Log.e("asdd", Tag + "onServiceConnected() ~ 성공!");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.e("asdd", Tag + "onServiceDisconnected() ~ ");
+            Log.e("asdd", Tag + "   - componentName :" + componentName);
+
+        }
+    };
 }
